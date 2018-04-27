51bd66dc32afd05497cb4b421627229ba2b628f7
WIN_NT\x17j078 <x17j078@chiba-fjb.ac.jp>
2018-04-27 11:47:03 +0900

    マージの確定をしました

--- src/Test.java
+++ src/Test.java
@@ -4,11 +4,11 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		 System.out.println("Gitテスト");
-<<<<<<< HEAD
+
 		 System.out.println("枝分かれ");
-=======
+
 		 System.out.println("追加");
->>>>>>> master
+
 	}
 
 }
